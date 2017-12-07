ca36989a57757f75c25d48372deebed937df3e04
Bamboo6 <dream1718@foxmail.com>
2017-12-08 03:27:41 +0800

    finish

--- application/index/controller/Search.php
+++ application/index/controller/Search.php
@@ -8,11 +8,10 @@
 
 namespace app\index\controller;
 
-
-use think\Controller;
 use think\Db;
+use think\View;
 
-class Search extends Controller
+class Search extends \think\Controller
 {
     /*public function search(){
 
@@ -30,21 +29,70 @@ class Search extends Controller
     public function search(){
         return $this->fetch();
     }
+    public function searchresult(){
+        return $this->fetch();
+    }
 
-    public function resule(){
+    public function searchdo(){
+        $view = new View();
+        $keyword=input('get.keyword');
+        $book=new \app\index\model\Book();
+        //这里我做的一个模糊查询到名字或者对应的id,主要目的因为我这个系统是
+        //商城系统里面用到直接看产品ID
+        $map['book_id|book_author|book_isbn|book_directory|book_about']  = array('like','%'.$keyword.'%');
+        $pageParam    = ['query' =>[]];
+        $pageParam['query']['keyword'] = $keyword;
+        $result=$book->where($map)->paginate(5,false, $pageParam);
+        $page = $result->render();
+        //var_dump($result);
+        $this->assign('result',$result);
+        $this->assign('page', $page);
+        $this->assign('keyword', $keyword);
+        /*header(strtolower("location: ".config('web_root')."/index/search/searchresult.html"));
+        exit();*/
+        $view->result = $result;
+        $view->keyword = $keyword;
+        $view->page = $page;
+
+        return $view->fetch('search/searchresult');
+    }
+    /*public function search1(){
+        $keyword = $_POST['search'];
+        $Goods=M('tb_book');
+        //这里我做的一个模糊查询到名字或者对应的id，主要目的因为我这个系统是
+        //商城系统里面用到直接看产品ID
+        $map['book_id|book_author|book_isbn|book_directory|book_about']  = array('like','%'.$keyword.'%');
+        // 把查询条件传入查询方法
+        if($goods=$Goods->where($map)->select())
+        {
+            $this->ajaxReturn($goods,'查询成功!',1);
+        }else{
+            $this->ajaxReturn(0,"查询失败,数据不存在！",0);
+        }
+    }*/
+    /*public function get_ajax_crmname(){
+
+        $keyword=input('post.keyword');
+        $map['book_id|book_author|book_isbn|book_directory|book_about']  = array('like','%'.$keyword.'%');
+
+        $result=M('tb_book')->where($map)->select();
+        var_dump($result);
+        $this->ajaxReturn($result);
+    }*/
+    /*public function result(){
         $keyword = $_GET['search'];
         $re=test($keyword);
         echo json_encode($re);//输出查询的结果（json格式输出）
         var_dump(json_encode($re));
-    }
+    }*/
 
-    public function test($keyword){//从数据库中查找关键字的函数
+    /*public function test($keyword){//从数据库中查找关键字的函数
         $u = new \app\index\model\Book();
         //这里我做的一个模糊查询到名字或者对应的id,主要目的因为我这个系统是
         //商城系统里面用到直接看产品ID
         $map['book_id|book_author|book_isbn|book_directory|book_about']  = array('like','%'.$keyword.'%');
         $result=$u->where($map)->select();
         return $result->fetchAll(PDO::FETCH_ASSOC);
-    }
+    }*/
 
 }
